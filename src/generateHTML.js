3dbd11710c96cc6bf2bd4b8bcb55191799f6473d
vayne <miguel.osorio19@yahoo.com>
2022-01-23 21:21:39 -0600

    incorporated some css and added different Icons based on role

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -39,7 +39,7 @@ const generateTheManagerCard = function (manager) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${manager.name}</h3>
-                <h4>Manager</h4><i class="material-icons">face</i>
+                <h4>Manager</h4><i class="material-icons">sports_bar</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${manager.id}</p>
@@ -58,7 +58,7 @@ const generateTheEngineerCard = function (engineer) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${engineer.name}</h3>
-                <h4>Engineer</h4><i class="material-icons">face</i>
+                <h4>Engineer</h4><i class="material-icons">code</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${engineer.id}</p>
@@ -77,7 +77,7 @@ const generateTheInternCard = function (intern) {
         <div class="card h-100">
             <div class="card-header">
                 <h3>${intern.name}</h3>
-                <h4>Intern</h4><i class="material-icons">face</i>
+                <h4>Intern</h4><i class="material-icons">headset</i>
             </div>
             <div class="card-body">
                 <p class="id">ID: ${intern.id}</p>
@@ -101,6 +101,9 @@ const generateTeamProfilePage = function(teamCards) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
         <title>Team Profile</title>
     </head>
     <body>
